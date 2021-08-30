601426eedd4c9eced4c375f1fc8e31223b2a86d3
positive-season <scp94373@gmail.com>
2021-08-30 18:03:51 +0100

    Finished android app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.catley.bmi',
+  appName: 'Check your BMI',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
